c42810416ea390c12aee5f849fed4c85205f0fe2
prasanjitsarker3 <sarkerprasanjit379@gmail.com>
2023-05-19 17:39:29 +0530

    Code Font Data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //Middle ware  
 app.use(cors())
@@ -29,15 +29,48 @@ async function run() {
         const inventionCollection = client.db("inventionDB").collection("products");
         app.get('/allInvention', async (req, res) => {
             const result = await inventionCollection.find().toArray();
+
             res.send(result);
         })
-
+        app.get('/allInvention/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await inventionCollection.findOne(query);
+            res.send(result);
+        })
+        app.get('/myInvention/:email', async (req, res) => {
+            const query = { email: req.params.email }
+            const result = await inventionCollection.find(query).toArray();
+            res.send(result);
+        })
         app.post('/postInvention', async (req, res) => {
             const body = req.body;
             const result = await inventionCollection.insertOne(body);
             res.send(result)
         })
+        app.delete('/myInvention/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await inventionCollection.deleteOne(query);
+            res.send(result)
+        })
+        app.put('/invention/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+            const updateInvention = req.body;
+            const user = {
+                $set:{
 
+                    sellerName: updateInvention.sellerName,
+                    price: updateInvention.price,
+                    quantity: updateInvention.quantity,
+                    details: updateInvention.details
+                }
+            }
+            const result = await inventionCollection.updateOne(filter, user, options)
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
